eb88c2d6de3a577b65e06b28ec01250599176519
gichanGim <k.kychan2003@gmail.com>
2026-07-03 21:01:29 +0900

    feat: 도메인 모듈 구조 및 Modulithic 루트 설정 선언 (#16)

--- src/main/java/org/sopt/hashi/HashiApplication.java
+++ src/main/java/org/sopt/hashi/HashiApplication.java
@@ -2,7 +2,9 @@ package org.sopt.hashi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
+@Modulithic(sharedModules = "auth")
 @SpringBootApplication
 public class HashiApplication {
 
